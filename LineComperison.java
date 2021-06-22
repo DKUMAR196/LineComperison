5bfec1d10db3a33e8fa0b12feb9f7ceaaedfe831
DKUMAR196 <deepakmecet@gmail.com>
2021-06-22 16:39:42 +0530

    Equality Of Two Line Equal

--- LineComperison.java
+++ LineComperison.java
@@ -1,29 +1,24 @@
+!#/bin/bash -x
 package LineComperison;
 
-import java.util.*;
-
-public class Line {
-	Point p1, p2;
+import java.util.Objects;
+public class Point {
+	float x, y;
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(p1, p2);
+		return Objects.hash(x, y);
 	}
 	@Override
 	public boolean equals(Object obj) {
-		/*if (this == obj)
+		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Line other = (Line) obj;
-		return Objects.equals(p1, other.p1) && Objects.equals(p2, other.p2);
-		*/
-		return this.getLength() == ((Line)obj).getLength();	
+		Point other = (Point) obj;
+		return Float.floatToIntBits(x) == Float.floatToIntBits(other.x)
+				&& Float.floatToIntBits(y) == Float.floatToIntBits(other.y);
 	}
-	public double getLength() {
-		return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y)));
-	}
-
 }
